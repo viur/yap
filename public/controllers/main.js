afbe9b64bb1b03111741fad0e050df36eae11b6a
David Custodio <davidlacustodio@gmail.com>
2014-10-09 18:02:27 +0100

    added gulp  bower install

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -18,4 +18,29 @@ app.controller('MainCtrl', ['$scope', 'geoService', function ($scope, geoService
         zoom: 8
     };
 
+    $scope.todos = [
+        {
+            face : '/img/list/60.jpeg',
+            what: 'Party Hard!',
+            category: 'Event',
+            when: '3:08PM',
+            notes: "Elm Street no.13"
+        },
+        {
+            face : '/img/list/60.jpeg',
+            what: 'City tour',
+            category: 'Tourism',
+            when: '3:08PM',
+            notes: "City hall"
+        },
+        {
+            face : '/img/list/60.jpeg',
+            what: 'Cops!',
+            category: 'Community',
+            when: '3:08PM',
+            notes: "Windy road"
+        }
+
+    ]
+
 }]);
