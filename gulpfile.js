3b1e6c2f19d4bcd62140a7bd08f79b9a7a0ab752
David Custodio <davidlacustodio@gmail.com>
2014-10-09 16:50:10 +0100

    added gulp with livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    embedlr = require("gulp-embedlr");
+    livereload = require('gulp-livereload');
+
 
 gulp.task('html', function () {
     gulp.src('public/**/*.html')
-        .pipe(embedlr())
+
         .pipe(livereload());
 });
 
@@ -12,6 +12,7 @@ gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('public/**').on('change', livereload.changed);
 });
+
 
 
 
