9095855ab99fc0b2678376536c4e4ebabd7422b8
Pedro Almeida <pedro@viur.pt>
2014-10-09 18:02:24 +0100

    base angular material

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,21 +1,21 @@
 app.controller('MainCtrl', ['$scope', 'geoService', function ($scope, geoService) {
 
-
+    $scope.center = {};
     geoService.getCurrentPosition().then(function (pos) {
 
         console.log(pos);
 
-        $scope.where = 'lat:' + pos.coords.latitude + ' lon:' + pos.coords.longitude;
+        $scope.where = 'lat:' + pos.coords.latitude + ' lng:' + pos.coords.longitude;
+
+        $scope.center = {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+            zoom: 20
+        }
 
 
     });
 
-    $scope.map = {
-        center: {
-            latitude: 45,
-            longitude: -73
-        },
-        zoom: 8
-    };
+
 
 }]);
